bc25fbd8e0a1fd711d5a5c4dc6fe16fe515116db
Ananthan493 <ananthanofficial493@gmail.com>
2024-06-07 21:30:40 +0530

    routed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -12,16 +13,16 @@ const Navbar = () => {
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Add Book</a>
+          <Link class="nav-link active" aria-current="page" to="/">Add Book</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/search">Search</a>
+          <Link class="nav-link" to="/search">Search</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/delete">Delete</a>
+          <Link class="nav-link" to="/delete">Delete</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/viewall">ViewAll</a>
+          <Link class="nav-link" to="/viewall">ViewAll</Link>
         </li>
       </ul>
     </div>
